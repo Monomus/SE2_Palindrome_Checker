4bc32ff8675af31440628fae96cf3ea21ffaff31
Christof Bachmann <christof.bachmann@gmail.com>
2018-03-06 19:55:26 +0100

    Initial Commit
    Created Project

---
+++ app/src/main/java/se2/aau/chris/se2_palindrome_checker/PalindromeChecker.java
@@ -0,0 +1,13 @@
+package se2.aau.chris.se2_palindrome_checker;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PalindromeChecker extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_palindrome_checker);
+    }
+}
